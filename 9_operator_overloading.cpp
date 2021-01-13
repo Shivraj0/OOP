b69ec44d55033042053fcc94b0c1b01b24f23c17
Shivraj <54741030+Shivraj0@users.noreply.github.com>
2021-01-13 23:48:29 +0530

    Added function overloading

---
+++ 9_operator_overloading.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class Number {
+    public: 
+        int num;
+
+    Number() {}
+
+    Number(int value) {
+        num = value;
+    }
+
+    Number operator+ (Number numObj)  {     // operator overloading function.
+        Number result;
+        result.num = num + numObj.num;
+        return result;
+    }
+};
+
+int main() {
+    Number obj1(2);
+    Number obj2(3);
+    Number obj3;
+
+    obj3 = obj1 + obj2;                     // operator overloading function called.
+    cout<<obj3.num;
+}
