28c8e1c40d76b766861ea1c2cd8217ccafbfe8e0
Shivraj <54741030+Shivraj0@users.noreply.github.com>
2021-01-16 00:43:27 +0530

    Added member functions.

---
+++ 14_member_initializer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class Person {
+    public:
+        Person(int a, int b);
+    
+    private:
+        int regVar;
+        const int constVar;
+};
+
+Person::Person(int a, int b) : constVar(a) {    // Const variable `cosntVar` initialized with value a.
+    cout<<"Constant variable: "<<constVar;
+}
+
+int main() {
+    Person personObj(1,2);
+}
