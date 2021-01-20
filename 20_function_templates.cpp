956e1541186dd69b7cfc9436b2a5c0cb10ada30f
Shivraj <54741030+Shivraj0@users.noreply.github.com>
2021-01-21 01:03:33 +0530

    Added funtion templates - multiple parameters.

--- 20_function_templates.cpp
+++ 20_function_templates.cpp
@@ -8,7 +8,17 @@ T add(T a, T b) {                 // Return type and funtion parameters are inde
     return a+b;
 }
 
+template <class S, class U>
+
+S ADD(S a, U b) {
+    return a + b;                 // Returns value of type S. i.e type of first parameter.
+}
+
 int main() {
     int a = 10, b = 20;
-    cout<<add(a,b);
+    cout<<add(a,b);              // Regular template with single class.
+    
+    int c = 10;
+    double d = 20.20;
+    cout<<ADD(c, d);            // Template with multiple input type parameters.
 }
