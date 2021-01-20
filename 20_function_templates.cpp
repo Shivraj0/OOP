fe5febce4adb3bef70ca6126fbfc49dec56b3ee2
Shivraj <54741030+Shivraj0@users.noreply.github.com>
2021-01-21 00:59:55 +0530

    Updated indentation

--- 20_function_templates.cpp
+++ 20_function_templates.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 template <class T>                // Create generic class. Generic class is basically for type of data instead of value.
 
-T add(T a, T b) {     // Return type and funtion parameters are independent of type but should be of same type i.e int, int.
+T add(T a, T b) {                 // Return type and funtion parameters are independent of type but should be of same type i.e int, int.
     return a+b;
 }
 
